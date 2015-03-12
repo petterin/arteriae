3ff33490ecd7ac15c6f0e9f8341c5ae0e535b919
Lauri Pullinen <lauri.pullinen@aalto.fi>
2015-03-12 09:50:15 +0200

    added a script which follows a given object's rotation with a given speed

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollower : MonoBehaviour {
+
+	public GameObject TargetCamera;
+	public float RotationSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.TargetCamera) {
+			this.transform.rotation =
+				Quaternion.Slerp(this.transform.rotation,
+								 this.TargetCamera.transform.rotation,
+								 this.RotationSpeed);
+		}
+	}
+}
