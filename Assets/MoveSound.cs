68662dd888a54743ee0bc50f3200283c4c64d0d2
Juho Salmio <juho.salmio@gmail.com>
2015-03-26 15:51:58 +0200

    change position change sound

--- Assets/MoveSound.cs
+++ Assets/MoveSound.cs
@@ -4,52 +4,56 @@ using System.Collections;
 public class MoveSound : MonoBehaviour {
 	
 	private bool isMoving;
-	private LimitedQueue<Quaternion> lastRotations;
-	public AudioClip MoveStartSound;
+	private bool fadingOut;
+	private LimitedQueue<Vector3> lastLocations;
 	public AudioClip MoveInProgressSound;
-	public AudioClip MoveEndSound;
 	public AudioSource AudioS;
 	public float AudioVolume = 1.0f;
-
+	private double locationThreshold = 0.3;
+	
 	// Use this for initialization
 	void Start () {
 		isMoving = false;
-		lastRotations = new LimitedQueue<Quaternion> (10);
+		lastLocations = new LimitedQueue<Vector3> (10);
+		lastLocations.Enqueue (this.transform.position);
 	}
 	
 	// Update is called once per frame
+
 	void Update () {
-		Quaternion currentRotation = this.transform.rotation;
-		lastRotations.Enqueue (currentRotation);
-		if (stoppingMove (isMoving, lastRotations.Peek(), currentRotation)) {
+		Vector3 currentLocation = this.transform.position;
+		lastLocations.Enqueue (currentLocation);
+		if (stoppingMove (isMoving, lastLocations.Peek(), currentLocation)) {
 			isMoving = false;
-			AudioS.loop = false;
-			AudioS.Pause();
-			AudioS.PlayOneShot(MoveEndSound, AudioVolume);
-
+			fadingOut = true;
 		}
-		else if (startingMove (isMoving, lastRotations.Peek(), currentRotation)) {
+		else if (startingMove (isMoving, lastLocations.Peek(), currentLocation)) {
 			isMoving = true;
-			AudioS.PlayOneShot(MoveStartSound, AudioVolume);
-			StartCoroutine(startMoveInProgressAfterStartSound());
+			fadingOut = false;
+			AudioS.loop = true;
+			AudioS.clip = MoveInProgressSound;
+			AudioS.volume = AudioVolume;
+			AudioS.Play();
+		}
+
+		if (fadingOut) {
+			AudioS.volume = AudioS.volume - 0.005f;
+			
+			if(AudioS.volume <= 0){
+				AudioS.Pause();
+				fadingOut = false;
+			}
 		}
 	}
+	
 
-	IEnumerator startMoveInProgressAfterStartSound() {
-		yield return new WaitForSeconds(MoveStartSound.length);
-		AudioS.loop = true;
-		AudioS.clip = MoveInProgressSound;
-		AudioS.volume = AudioVolume;
-		AudioS.Play();
+		
+	bool stoppingMove(bool isMoving, Vector3 locationA, Vector3 locationB) {
+		return isMoving && Vector3.Distance(locationA, locationB) <= locationThreshold;	
 	}
-
-
-	bool stoppingMove(bool isMoving, Quaternion rotationA, Quaternion rotationB) {
-		return isMoving && rotationA == rotationB;	
+	
+	bool startingMove(bool isMoving, Vector3 locationA, Vector3 locationB) {
+		return !isMoving && !fadingOut && Vector3.Distance(locationA, locationB) > 0.5;
 	}
-
-	bool startingMove(bool isMoving, Quaternion rotationA, Quaternion rotationB) {
-		return !isMoving && rotationA != rotationB;	
-	}
-
+	
 }
---
+++ Assets/RotationSound.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationSound : MonoBehaviour {
+	
+	private bool isMoving;
+	private LimitedQueue<Quaternion> lastRotations;
+	public AudioClip MoveStartSound;
+	public AudioClip MoveInProgressSound;
+	public AudioClip MoveEndSound;
+	public AudioSource AudioS;
+	public float AudioVolume = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		isMoving = false;
+		lastRotations = new LimitedQueue<Quaternion> (10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion currentRotation = this.transform.rotation;
+		lastRotations.Enqueue (currentRotation);
+		if (stoppingMove (isMoving, lastRotations.Peek(), currentRotation)) {
+			isMoving = false;
+			AudioS.loop = false;
+			AudioS.Pause();
+			AudioS.PlayOneShot(MoveEndSound, AudioVolume);
+
+		}
+		else if (startingMove (isMoving, lastRotations.Peek(), currentRotation)) {
+			isMoving = true;
+			AudioS.PlayOneShot(MoveStartSound, AudioVolume);
+			StartCoroutine(startMoveInProgressAfterStartSound());
+		}
+	}
+
+	IEnumerator startMoveInProgressAfterStartSound() {
+		yield return new WaitForSeconds(MoveStartSound.length);
+		AudioS.loop = true;
+		AudioS.clip = MoveInProgressSound;
+		AudioS.volume = AudioVolume;
+		AudioS.Play();
+	}
+
+
+	bool stoppingMove(bool isMoving, Quaternion rotationA, Quaternion rotationB) {
+		return isMoving && rotationA == rotationB;	
+	}
+
+	bool startingMove(bool isMoving, Quaternion rotationA, Quaternion rotationB) {
+		return !isMoving && rotationA != rotationB;	
+	}
+
+}
