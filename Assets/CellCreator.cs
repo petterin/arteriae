74689eedb6444c6aed2a7ff1fc6e0f65ba85420b
Juho Salmio <juho.salmio@gmail.com>
2015-03-30 23:42:46 +0300

    generate bloodcells manually, disable razer hydra

---
+++ Assets/CellCreator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellCreator : MonoBehaviour {
+
+	public GameObject cell;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn", 0, 3f);
+		
+	}
+	void Spawn () {
+		GameObject obj = (GameObject) Instantiate (cell, transform.position, transform.rotation);
+		Vector3 initialVelocity = Random.insideUnitSphere * 10;
+		obj.GetComponent<Rigidbody>().velocity = initialVelocity;
+	}
+
+}
