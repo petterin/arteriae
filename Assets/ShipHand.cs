e725dcfca74c3a16a58f58a133e2ca864c537099
Lauri Pullinen <lauri.pullinen@aalto.fi>
2015-05-02 12:50:21 +0300

    ship hand movement from parent

--- Assets/ShipHand.cs
+++ Assets/ShipHand.cs
@@ -3,25 +3,16 @@ using System.Collections;
 
 public class ShipHand : MonoBehaviour {
 	public KeyCode control;
-	public GameObject parent;
-	private Vector3 originalPosition;
-	private Vector3 translationOffset;
 	
 	private bool closed;
 
 	// Use this for initialization
 	void Start () {
 		this.closed = false;
-		this.originalPosition = this.parent.transform.InverseTransformPoint(this.transform.position);
-		//this.translationOffset = this.transform.position - this.parent.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(this.parent != null) {
-			this.transform.position = this.parent.transform.TransformPoint(this.originalPosition/* TODO: Razer hydra position delta */);
-			//this.transform.position = this.translationOffset + this.parent.transform.position;
-		}
 		if(Input.GetKey(this.control)) {
 			if(!this.closed)
 				Debug.Log("Closing hand");
