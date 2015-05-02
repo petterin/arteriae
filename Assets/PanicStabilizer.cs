eb5466c58906c60a17b2154c5740df444ada7138
Juho Salmio <juho.salmio@gmail.com>
2015-05-02 15:59:57 +0300

    ship stabilizer

---
+++ Assets/PanicStabilizer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanicStabilizer : MonoBehaviour {
+	public KeyCode Control;
+	public GameObject shipModel;
+	// Use this for initialization
+	
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(this.Control)) {
+			Debug.Log (this.gameObject.transform.rotation);
+			this.shipModel.transform.rotation = Quaternion.identity;
+
+		}
+	
+	}
+}
