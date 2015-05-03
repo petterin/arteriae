49ab2e4c87a5ca852396d789a530dbd92aea2f63
Juho Salmio <juho.salmio@gmail.com>
2015-05-03 17:07:20 +0300

    goal sound and restart

---
+++ Assets/Goal.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	public AudioSource Source;
+	public GameObject Player;
+	private bool playedOnce;
+
+	// Use this for initialization
+	void Start () {
+		playedOnce = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject == Player)
+		{
+			if(!Source.isPlaying || !playedOnce){
+				Source.Play();
+				playedOnce = true;
+				StartCoroutine(WaitAndRestart());
+
+			}
+				
+		}
+	}
+
+	IEnumerator WaitAndRestart() {
+		yield return new WaitForSeconds(Source.clip.length + 3);
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
