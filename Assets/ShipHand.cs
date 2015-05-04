de7e80d25e41206daeea6eba2e03f85dd9bab1a5
Lauri Pullinen <lauri.pullinen@aalto.fi>
2015-05-04 18:28:09 +0300

    added pause key to ship movement script

--- Assets/ShipHand.cs
+++ Assets/ShipHand.cs
@@ -6,8 +6,19 @@ public class ShipHand : MonoBehaviour {
 	public SixenseHands Hand;
 	// Used for debugging mainly
 	public KeyCode Control;
+	public Collider ClawCollider;
+	
+	public GameObject ClawLeft;
+	public GameObject ClawRight;
+	public Vector3 ClawPivotLeft;
+	public Vector3 ClawPivotRight;
+	
+	public float CloseSpeed = 10.0f;
+	public float MaxAngleOfClaws;
 	
 	private bool closed;
+	
+	private float currentRotation;
 
 	// Use this for initialization
 	void Start () {
@@ -17,19 +28,49 @@ public class ShipHand : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		SixenseInput.Controller controller = SixenseInput.GetController(Hand);
+		
+		Vector3 pivotLeft = this.ClawLeft.transform.TransformPoint(this.ClawPivotLeft);
+		Vector3 pivotRight = this.ClawRight.transform.TransformPoint(this.ClawPivotRight);
+		
+		float angle = Time.deltaTime * this.CloseSpeed;
+		
 		// NOTE: the trigger button threshold can be set through SixenseInput
 		if(Input.GetKey(this.Control) || (controller != null && controller.GetButton(SixenseButtons.TRIGGER))) {
 			if(!this.closed)
 				Debug.Log("Closing hand");
 			this.closed = true;
+			if(this.currentRotation + angle <= this.MaxAngleOfClaws) {
+				this.currentRotation += angle;
+			} else {
+				angle = this.MaxAngleOfClaws - this.currentRotation;
+				this.currentRotation = this.MaxAngleOfClaws;
+			}
+			this.ClawLeft.transform.RotateAround(pivotLeft, this.ClawLeft.transform.up, angle);
+			this.ClawRight.transform.RotateAround(pivotRight, this.ClawRight.transform.up, -angle);
 		} else {
 			if(this.closed)
 				Debug.Log("Opening hand");
 			this.closed = false;
+			if(this.currentRotation - angle >= 0) {
+				this.currentRotation -= angle;
+			} else {
+				angle = this.currentRotation;
+				this.currentRotation = 0;
+			}
+			this.ClawLeft.transform.RotateAround(pivotLeft, this.ClawLeft.transform.up, -angle);
+			this.ClawRight.transform.RotateAround(pivotRight, this.ClawRight.transform.up, angle);
 		}
 	}
 	
 	public bool IsClosed() {
 		return this.closed;
 	}
+	/*
+	void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawSphere(ClawLeft.transform.TransformPoint(this.ClawPivotLeft), 1.5f);
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(ClawRight.transform.TransformPoint(this.ClawPivotRight), 1.5f);
+	}
+	*/
 }
--- Assets/ShipMovement.cs
+++ Assets/ShipMovement.cs
@@ -16,8 +16,12 @@ public class ShipMovement : MonoBehaviour {
 	public float RotationThresholdAngle = 25;
 	
 	public bool EnableRoll = true;
+	public bool StartPaused = true;
+	public KeyCode PauseKey = KeyCode.X;
 	
 	public Vector3 centerOfMass;
+	
+	private bool paused;
 	
 	private KeyCode MoveForwardKey = KeyCode.W;
 	private KeyCode MoveBackwardKey = KeyCode.S;
@@ -40,80 +44,91 @@ public class ShipMovement : MonoBehaviour {
 			Debug.LogWarning("No SixenseInput found for the ShipMovement script! Only keyboard commands are available: WASD, IKJL", this);
 		}
 		this.Ship.GetComponent<Rigidbody>().centerOfMass = this.centerOfMass;
+		this.paused = this.StartPaused;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		Vector3 direction = new Vector3(0,0,0);
-		
-		// Handling translation first
-		if(this.SixenseInputObject != null && this.SixenseInputObject.activeInHierarchy) {
-			// The left joystick is used for movement in the xz plane
-			SixenseInput.Controller leftController = SixenseInput.GetController(SixenseHands.LEFT);
-			// Moving up and down is controlled by the right handle's buttons 4 and 3
-			SixenseInput.Controller rightController = SixenseInput.GetController(SixenseHands.RIGHT);
+		if(!this.paused) {
+			Vector3 direction = new Vector3(0,0,0);
 			
-			if(leftController == null || rightController == null) {
-				Debug.Log("Could not find Sixense controllers!");
-			} else {		
-				direction = this.Ship.transform.forward * leftController.JoystickY +
-					this.Ship.transform.right * leftController.JoystickX;
+			// Handling translation first
+			if(this.SixenseInputObject != null && this.SixenseInputObject.activeInHierarchy) {
+				// The left joystick is used for movement in the xz plane
+				SixenseInput.Controller leftController = SixenseInput.GetController(SixenseHands.LEFT);
+				// Moving up and down is controlled by the right handle's buttons 4 and 3
+				SixenseInput.Controller rightController = SixenseInput.GetController(SixenseHands.RIGHT);
 				
-				direction += this.Ship.transform.up * rightController.JoystickY;
+				if(leftController == null || rightController == null) {
+					Debug.Log("Could not find Sixense controllers!");
+				} else {		
+					direction = this.Ship.transform.forward * leftController.JoystickY +
+						this.Ship.transform.right * leftController.JoystickX;
+					
+					direction += this.Ship.transform.up * rightController.JoystickY;
+					if(this.EnableRoll) {
+						Vector3 torque = this.Ship.transform.forward * 
+							this.RotationPower * 
+							-rightController.JoystickX;
+						this.Ship.GetComponent<Rigidbody>().AddTorque(getTorqueWithoutInertiaTensor(torque));
+					}
+				}
+			} else {
+				if(Input.GetKey(this.MoveForwardKey)) {
+					direction += this.Ship.transform.forward;
+				}
+				if(Input.GetKey(this.MoveBackwardKey)) {
+					direction -= this.Ship.transform.forward;
+				}
+				if(Input.GetKey(this.MoveLeftKey)) {
+					direction -= this.Ship.transform.right;
+				}
+				if(Input.GetKey(this.MoveRightKey)) {
+					direction += this.Ship.transform.right;
+				}
+				if(Input.GetKey(this.MoveUpKey)) {
+					direction += this.Ship.transform.up;
+				}
+				if(Input.GetKey(this.MoveDownKey)) {
+					direction -= this.Ship.transform.up;
+				}
 				if(this.EnableRoll) {
-					Vector3 torque = this.Ship.transform.forward * 
-						this.RotationPower * 
-						-rightController.JoystickX;
+					Vector3 torque = new Vector3(0,0,0);
+					if(Input.GetKey(this.RollLeftKey)) {
+						torque += this.Ship.transform.forward * this.RotationPower;
+					}
+					if(Input.GetKey(this.RollRightKey)) {
+						torque -= this.Ship.transform.forward * this.RotationPower;
+					}
 					this.Ship.GetComponent<Rigidbody>().AddTorque(getTorqueWithoutInertiaTensor(torque));
 				}
 			}
-		} else {
-			if(Input.GetKey(this.MoveForwardKey)) {
-				direction += this.Ship.transform.forward;
-			}
-			if(Input.GetKey(this.MoveBackwardKey)) {
-				direction -= this.Ship.transform.forward;
-			}
-			if(Input.GetKey(this.MoveLeftKey)) {
-				direction -= this.Ship.transform.right;
-			}
-			if(Input.GetKey(this.MoveRightKey)) {
-				direction += this.Ship.transform.right;
-			}
-			if(Input.GetKey(this.MoveUpKey)) {
-				direction += this.Ship.transform.up;
-			}
-			if(Input.GetKey(this.MoveDownKey)) {
-				direction -= this.Ship.transform.up;
-			}
-			if(this.EnableRoll) {
-				Vector3 torque = new Vector3(0,0,0);
-				if(Input.GetKey(this.RollLeftKey)) {
-					torque += this.Ship.transform.forward * this.RotationPower;
-				}
-				if(Input.GetKey(this.RollRightKey)) {
-					torque -= this.Ship.transform.forward * this.RotationPower;
-				}
-				this.Ship.GetComponent<Rigidbody>().AddTorque(getTorqueWithoutInertiaTensor(torque));
+			
+			direction.Normalize();
+			// Note: no deltaTime multiplier is necessary, the physics engine takes care of that
+			Rigidbody rigidbody = this.Ship.GetComponent<Rigidbody>();
+			rigidbody.AddForce(direction * this.Power);
+			
+			// Moving the camera with the ship
+			// Actually, rely on the oculus + kinect to get the position right
+			// this.Camera.transform.position = this.Ship.transform.position;
+			
+			// Then handling rotation
+			float angle = Vector3.Angle(this.Ship.transform.forward, this.Camera.transform.forward);
+			if(angle > this.RotationThresholdAngle) {
+				// Rotation axis is the cross product of the target and current forward vector
+				Vector3 axis = Vector3.Cross(this.Ship.transform.forward, this.Camera.transform.forward);
+				Vector3 torque = getTorqueWithoutInertiaTensor(axis * this.RotationPower);
+				this.Ship.GetComponent<Rigidbody>().AddTorque(torque);
 			}
 		}
-		
-		direction.Normalize();
-		// Note: no deltaTime multiplier is necessary, the physics engine takes care of that
-		Rigidbody rigidbody = this.Ship.GetComponent<Rigidbody>();
-		rigidbody.AddForce(direction * this.Power);
-		
-		// Moving the camera with the ship
-		this.Camera.transform.position = this.Ship.transform.position;
-		
-		// Then handling rotation
-		float angle = Vector3.Angle(this.Ship.transform.forward, this.Camera.transform.forward);
-		if(angle > this.RotationThresholdAngle) {
-			// Rotation axis is the cross product of the target and current forward vector
-			Vector3 axis = Vector3.Cross(this.Ship.transform.forward, this.Camera.transform.forward);
-			Vector3 torque = getTorqueWithoutInertiaTensor(axis * this.RotationPower);
-			this.Ship.GetComponent<Rigidbody>().AddTorque(torque);
-		}
+	}
+	
+	void Update() {
+		if(Input.GetKeyDown(this.PauseKey)) {
+			this.paused = !this.paused;
+			Debug.Log("Set paused: " + this.paused);
+		}	
 	}
 	
 	private Vector3 getTorqueWithoutInertiaTensor(Vector3 torque) {
